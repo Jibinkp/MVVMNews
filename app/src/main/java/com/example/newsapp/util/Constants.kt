11cb074698e3464d3cb4918200bfdd349a1c4af5
jibinkp <jibinkp770@gmail.com>
2026-05-30 14:47:18 +0530

    Retrofit setup

---
+++ app/src/main/java/com/example/newsapp/util/Constants.kt
@@ -0,0 +1,8 @@
+package com.example.newsapp.util
+
+class Constants {
+    companion object {
+        const val API_KEY = "943b2a4bf6754f158ee6ffeca6e28ea5"
+        const val BASE_URL = "https://newsapi.org"
+    }
+}
